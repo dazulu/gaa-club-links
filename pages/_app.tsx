41fc24c93d523fa7cfa71729a81dc2e3091b5b22
Adrian Payne <dazulu@users.noreply.github.com>
2022-05-26 18:11:41 +0200

    feat: add fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
 import "../styles/fonts.css";
 import "../styles/globals.css";
+
 import type { AppProps } from "next/app";
 
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as Fathom from "fathom-client";
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    Fathom.load("RPQCSQDX", {
+      includedDomains: ["clublinks.netlify.app"],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
